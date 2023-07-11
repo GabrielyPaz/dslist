f02c0b796d36b00eb049f8cd3e21c04be363f0f1
GabrielyPaz <gabrielysouza120@gmail.com>
2023-07-11 16:30:16 -0300

    Project created

---
+++ src/main/java/com/example/dslistjavaspring/DslistJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.dslistjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistJavaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslistJavaSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dslistjavaspring/DslistJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dslistjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistJavaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
